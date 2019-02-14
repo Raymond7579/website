d1339d424d1fe4d56652ffe89b19d5248b881250
Raymond7579 <47564862+Raymond7579@users.noreply.github.com>
2019-02-14 14:06:59 +0000

    Create loginform.php

---
+++ loginform.php
@@ -0,0 +1,22 @@
+<form action="login.php" method="POST">
+  <table>
+  <tr>
+    <td>
+      Name:</td>
+    <td>
+      <input type="text" placeholder="name" nem="username" required>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      Password:</td>
+    </td>
+    <td>
+      <input type="password" placeholder="password" name="password" required>
+      </td>
+    </tr>
+      <td> 
+        <input type="submit" value="submit" name="">
+      </td>
+    </table>
+    </form>
